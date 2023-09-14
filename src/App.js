19c9c866efbed6d49e625b6dc66863d8d02bd591
Krzysztof Staszewski <krzysiek.staszewski@gmail.com>
2023-09-14 15:20:09 +0200

    first-react-app init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Hero from './components/Hero/Hero'
+import SearchForm from './components/SearchForm/SearchForm'
+import List from './components/List/List';
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <SearchForm />
+      <List />
+    </div>
+  );
+};
+
+export default App;
