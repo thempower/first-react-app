b5f95f5e10e71aed0cab3163ddeea89c22833861
Krzysztof Staszewski <krzysiek.staszewski@gmail.com>
2023-09-19 08:19:45 +0200

    finished props children wrapper container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Hero from './components/Hero/Hero'
 import SearchForm from './components/SearchForm/SearchForm'
 import List from './components/List/List';
+import Container from './components/Container/Container';
 const App = () => {
   return (
-    <div>
+    <Container>
       <Hero />
       <SearchForm />
       <List />
-    </div>
+    </Container>
   );
 };
 
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,10 +5,12 @@ import Button from '../Button/Button';
 const SearchForm = () => {
     return (
         <form className={styles.searchForm}>
-            <TextInput placeholder ="Search..." />
-            <Button text="Create" />
+            <TextInput placeholder="Search..." />
+            <Button>
+                <span className="fa fa-search" />
+            </Button>
         </form>
     );
-};
+  };
 
 export default SearchForm;
