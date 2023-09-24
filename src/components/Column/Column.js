92de9788c9620405a32d9fdd4d4280d71c7ea01f
Krzysztof Staszewski <krzysiek.staszewski@gmail.com>
2023-09-24 18:06:40 +0200

    init CardForm

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,9 +1,15 @@
-import styles from './Column.module.scss'
+import styles from './Column.module.scss';
+import Card from '../Card/Card.js';
 
 const Column = props => {
+
+
     return (
         <article className={styles.column}>
             <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} /> {props.title}</h2>
+            <ul className={styles.cards}>
+                {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+            </ul>
         </article>
     );
 };
